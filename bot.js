c769f53f77e278a049dc80dcb2d2387d473e99ec
rusak007 <74760377+rusak007@users.noreply.github.com>
2022-08-05 18:18:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
-    username: "Test",
+    host: "createmodBR.aternos.me",
+    port: "36174",
+    username: "Botyara",
     version: false
     })
     bot.on('login', function() {
